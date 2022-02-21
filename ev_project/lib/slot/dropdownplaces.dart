b14dd6a05a31c33c9c8f9226e346529756654d69
Venkatesan <61541134+VenkatSAIWorld@users.noreply.github.com>
2022-02-21 22:26:47 +0530

    Update dropdownplaces.dart

--- ev_project/lib/slot/dropdownplaces.dart
+++ ev_project/lib/slot/dropdownplaces.dart
@@ -15,8 +15,8 @@ class _DropDownPlacesState extends State<DropDownPlaces> {
     "Bangalore": ["Koramangala", "Madiwala", "Santhi Nagar"]
   };
 
-  var cty = "Chennai";
-  var loc = "Guindy";
+  var cty;
+  var loc;
   var index = 0;
 
   _count() {
@@ -32,8 +32,9 @@ class _DropDownPlacesState extends State<DropDownPlaces> {
         children: [
           Container(
             child: DropdownButton(
+              hint: Text("Nearest City"),
               value: cty,
-              items: Cities.map<DropdownMenuItem<String>>(
+              items: locations_City_list.map<DropdownMenuItem<String>>(
                 (String? value1) {
                   return DropdownMenuItem(
                     value: value1,
@@ -44,31 +45,35 @@ class _DropDownPlacesState extends State<DropDownPlaces> {
                   );
                 },
               ).toList(),
-              onChanged: (String? val) {
+              onChanged: (val) async {
                 setState(() {
-                  cty = val!;
+                  cty = val;
                 });
+                if (locations_City_list.contains(cty)) {
+                  index = locations_City_list.lastIndexOf(cty);
+                }
               },
             ),
           ),
           Ev("Nearest Stations"),
           Container(
             child: DropdownButton(
+              hint: Text("Nearest Stations"),
               value: loc,
-              items: locations[cty].map<DropdownMenuItem<String>>(
-                (String? value2) {
+              items: loc_list[index].Places?.map<DropdownMenuItem<String>>(
+                (value) {
                   return DropdownMenuItem(
-                    value: value2,
+                    value: value,
                     child: Text(
-                      value2!,
+                      value,
                       style: TextStyle(color: Colors.green[800]),
                     ),
                   );
                 },
               ).toList(),
-              onChanged: (String? val2) {
+              onChanged: (val2) {
                 setState(() {
-                  loc = val2!;
+                  loc = val2;
                 });
               },
             ),
@@ -78,3 +83,34 @@ class _DropDownPlacesState extends State<DropDownPlaces> {
     );
   }
 }
+
+class Locations {
+  String? City;
+  List? Places;
+  Locations({this.City, this.Places});
+}
+
+Locations loc1 =
+    Locations(City: "Chennai", Places: ["TNagar", "Saidapet", "Guindy"]);
+Locations loc2 = Locations(
+    City: "Bangalore", Places: ["Koramangala", "Madiwala", "Shanthi Nagar"]);
+Locations loc3 = Locations(
+    City: "Trichy",
+    Places: ["Chattiram Bus Stand", "Somarasempettai", "Sri Rangam"]);
+Locations loc4 = Locations(
+    City: "Madurai", Places: ["Periyar Bus Stand", "Thirumangalam", "Mallur"]);
+Locations loc5 =
+    Locations(City: "Salem", Places: ["Ammapettai", "Suramangalam", "5 Roads"]);
+Locations loc6 =
+    Locations(City: "Coimbatore", Places: ["Gandhipuram", "Ukkadam", "Sulur"]);
+
+List<String> locations_City_list = [
+  loc1.City!,
+  loc2.City!,
+  loc3.City!,
+  loc4.City!,
+  loc5.City!,
+  loc6.City!
+];
+
+List<Locations> loc_list = [loc1, loc2, loc3, loc4, loc5, loc6];
